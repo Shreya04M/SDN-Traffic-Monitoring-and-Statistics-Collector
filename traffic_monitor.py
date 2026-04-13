528f6515ca3232ebedd05b208776ddd022beab68
Shreya <shreyamattimadu@gmail.com>
2026-04-13 22:29:42 +0530

    modified comments

--- traffic_monitor.py
+++ traffic_monitor.py
@@ -15,7 +15,7 @@ class TrafficMonitor(app_manager.RyuApp):
         self.mac_to_port = {}  #stores port
         self.monitor_thread = hub.spawn(self.monitor) #starts bg monitor loop and runs every few seconds 
 
-    # Register switch
+    # Registeration of  switch
     @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER]) 
     #triggers when switches are called /connected
 
@@ -26,16 +26,17 @@ class TrafficMonitor(app_manager.RyuApp):
             if datapath.id not in self.datapaths:
                 print(f"Register datapath: {datapath.id}")
                 self.datapaths[datapath.id] = datapath
-        elif ev.state == DEAD_DISPATCHER:
+        elif ev.state == DEAD_DISPATCHER: # if switch gets disconnected 
             if datapath.id in self.datapaths:
                 print(f"Unregister datapath: {datapath.id}")
-                del self.datapaths[datapath.id]
+                del self.datapaths[datapath.id] #remove switch
 
     # Monitor loop
     def monitor(self):
         while True:
             for dp in self.datapaths.values():
                 self.request_stats(dp)
+                #for every switch /loop asks stats
             hub.sleep(5)
 
     # Request flow stats
@@ -43,22 +44,28 @@ class TrafficMonitor(app_manager.RyuApp):
         ofproto = datapath.ofproto
         # used to creat openflow rules 
         parser = datapath.ofproto_parser
-
+#creates open flow messages 
         req = parser.OFPFlowStatsRequest(datapath)
         datapath.send_msg(req)
 #parsermatches packets 
+
+
     # Receive flow stats
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def flow_stats_reply_handler(self, ev):
-        body = ev.msg.body
+        body = ev.msg.body  #contains flow data
 
         print("\n--- Flow Statistics ---")
         for stat in sorted(body, key=lambda x: (x.match.get('in_port', 0))):
+            #sorts flow by input ports
+
             print(f"Match: {stat.match}, "
                   f"Packets: {stat.packet_count}, "
                   f"Bytes: {stat.byte_count}")
 
     # Packet-in handler (learning switch logic)
+
+    # runs when packet arrives 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
         msg = ev.msg
@@ -70,8 +77,9 @@ class TrafficMonitor(app_manager.RyuApp):
         self.mac_to_port.setdefault(dpid, {})
 
         pkt = packet.Packet(msg.data)
-        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)  # ethernet packet 
 
+#dest and src MAC address
         dst = eth_pkt.dst
         src = eth_pkt.src
 
@@ -85,16 +93,19 @@ class TrafficMonitor(app_manager.RyuApp):
             out_port = self.mac_to_port[dpid][dst]
         else:
             out_port = ofproto.OFPP_FLOOD
+            #braidcasting is done iin else part
 
+#action defines where packet goes 
         actions = [parser.OFPActionOutput(out_port)]
 
         # Install flow rule (if not flooding)
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst) # condition/protocols for parser 
             self.add_flow(datapath, 1, match, actions)
 
         # Send packet
         out = parser.OFPPacketOut(
+        #forward packets 
             datapath=datapath,
             buffer_id=msg.buffer_id,
             in_port=in_port,
@@ -104,14 +115,16 @@ class TrafficMonitor(app_manager.RyuApp):
         datapath.send_msg(out)
 
     # Add flow rule
+    #basically creates flow rules 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         inst = [parser.OFPInstructionActions(
-            ofproto.OFPIT_APPLY_ACTIONS, actions)]
+            ofproto.OFPIT_APPLY_ACTIONS, actions)] #these apply actions
 
         mod = parser.OFPFlowMod(
+        #flow rule object 
             datapath=datapath,
             priority=priority,
             match=match,
